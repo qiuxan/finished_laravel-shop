f5c0441246f816acd61d8d03c72989ce350f86ea
vagrant <qiuxan@gmail.com>
2018-08-20 02:48:33 +0000

    basic layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+           BY <a href="" target="_blank">Ian</a> Developed <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:root@leo108.com">Contact Us</a></p>
+    </div>
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', 'Home Page')
+
+@section('content')
+  <h1>This is Homepage</h1>
+@stop
