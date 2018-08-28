92e3c35c55255cde44b1106d5e3ca17ff0337929
vagrant <qiuxan@gmail.com>
2018-08-28 05:39:14 +0000

    user interface saved product

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,8 @@ Route::group(['middleware' => 'auth'], function() {
         Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
 
 
+        Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
+        Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
 
 
 
